5750797caf8b0dc0b13b0d294383729d392fb754
PopiDaCoder <popialtes10@gmail.com>
2024-10-06 22:35:49 +0200

    generación por usuario

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
-import characters.Clerigo;
-import characters.Enano;
-import characters.Guerrero;
-import characters.Mago;
+import characters.*;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Personaje de ejemplo:");
+
         PersonajeBuilder clerigoBuilder = new PersonajeBuilder(ClasePersonaje.Clerigo);
+        PersonajeBuilder elfoBuilder = new PersonajeBuilder(ClasePersonaje.Elfo);
+        PersonajeBuilder enanoBuilder = new PersonajeBuilder(ClasePersonaje.Enano);
+        PersonajeBuilder guerreroBuilder = new PersonajeBuilder(ClasePersonaje.Guerrero);
+        PersonajeBuilder ladronBuilder = new PersonajeBuilder(ClasePersonaje.Ladron);
+        PersonajeBuilder magoBuilder = new PersonajeBuilder(ClasePersonaje.Mago);
+        PersonajeBuilder medianoBuilder = new PersonajeBuilder(ClasePersonaje.Mediano);
+
         clerigoBuilder.setCarisma(13);
         clerigoBuilder.setConstitution(8);
         clerigoBuilder.setDestreza(10);
@@ -15,16 +24,51 @@ public class Main {
         Clerigo clerigo = (Clerigo) clerigoBuilder.build();
         clerigo.printInfo();
 
-        PersonajeBuilder enanoBuilder = new PersonajeBuilder(ClasePersonaje.Enano);
-        Enano enano = (Enano) enanoBuilder.buildRandom();
-        enano.printInfo();
+        int i = 1;
+        while(i == 1) {
+            System.out.println("\nGeneración de personajes:");
+            Scanner scanner = new Scanner(System.in);
 
-        PersonajeBuilder guerreoBuilder = new PersonajeBuilder(ClasePersonaje.Guerrero);
-        Guerrero guerrero = (Guerrero) guerreoBuilder.buildRandom();
-        guerrero.printInfo();
+            System.out.println("Para crear su personaje introduzca nombre de la clase deseada.");
+            System.out.println("Clases a escoger: Clerigo, Elfo, Enano, Guerrero, Ladron, Mago, Mediano.");
+            System.out.println("Para salir escriba: Salir");
+            System.out.println("Clase deseada:");
 
-        PersonajeBuilder magoBuilder = new PersonajeBuilder(ClasePersonaje.Mago);
-        Mago mago = (Mago) magoBuilder.buildRandom();
-        mago.printInfo();
+            String generador = scanner.nextLine();
+
+            switch (generador) {
+                case "Clerigo":
+                    Clerigo C1 = (Clerigo) clerigoBuilder.buildRandom();
+                    C1.printInfo();
+                    break;
+                case "Elfo":
+                    Elfo El1 = (Elfo) elfoBuilder.buildRandom();
+                    El1.printInfo();
+                    break;
+                case "Enano":
+                    Enano En1 = (Enano) enanoBuilder.buildRandom();
+                    En1.printInfo();
+                    break;
+                case "Guerrero":
+                    Guerrero G1 = (Guerrero) guerreroBuilder.buildRandom();
+                    G1.printInfo();
+                    break;
+                case "Ladron":
+                    Ladron L1 = (Ladron) ladronBuilder.buildRandom();
+                    L1.printInfo();
+                    break;
+                case "Mago":
+                    Mago Ma1 = (Mago) magoBuilder.buildRandom();
+                    Ma1.printInfo();
+                    break;
+                case "Mediano":
+                    Mediano Me1 = (Mediano) medianoBuilder.buildRandom();
+                    Me1.printInfo();
+                    break;
+                case "Salir":
+                    i = 0;
+            }
+
+        }
     }
 }
